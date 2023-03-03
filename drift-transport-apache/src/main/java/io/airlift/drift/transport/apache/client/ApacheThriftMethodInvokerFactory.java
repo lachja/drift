26412ba7d75937de5a54db3ea9095804cb87a3a1
Jan Lachmuth <janlachmuth@lundus.de>
2023-03-03 22:59:08 +0100

    update libthrift

--- drift-transport-apache/src/main/java/io/airlift/drift/transport/apache/client/ApacheThriftMethodInvokerFactory.java
+++ drift-transport-apache/src/main/java/io/airlift/drift/transport/apache/client/ApacheThriftMethodInvokerFactory.java
@@ -24,8 +24,8 @@ import io.airlift.security.pem.PemReader;
 import org.apache.thrift.protocol.TBinaryProtocol;
 import org.apache.thrift.protocol.TCompactProtocol;
 import org.apache.thrift.protocol.TProtocolFactory;
-import org.apache.thrift.transport.TFramedTransport;
 import org.apache.thrift.transport.TTransportFactory;
+import org.apache.thrift.transport.layered.TFramedTransport;
 
 import javax.annotation.PreDestroy;
 import javax.inject.Inject;
